6bbe34cd29b09ecfffafd719673de9a895a2c348
johnvincentio <john@johnvincent.io>
2020-04-29 01:41:29 -0400

    add

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -7,114 +7,34 @@ import PropTypes from 'prop-types';
 
 import Grid from './Grid';
 
+import { initialize, updateGrid, isComplete } from '../utils';
+
 class Board extends React.Component {
 	constructor(props) {
 		super(props);
-		const { grid, moves } = this.init(props.gridSize);
-		this.state = {
-			complete: false,
-			grid,
-			moves,
-			clicked: { status: false }
-		};
-	}
-
-	init = gridSize => {
-		let arr = new Array(gridSize);
-		for (let i = 0; i < arr.length; i++) {
-			const columns = new Array(gridSize);
-			for (let j = 0; j < columns.length; j++) {
-				columns[j] = { id: i * arr.length + j, column: j, lightson: false };
-			}
-			arr[i] = { row: i, columns };
-		}
-		const moves = [];
-		// const maxMoves = props.gridSize ** 2;
-		const maxMoves = 2;
-		for (let i = 0; i < maxMoves; i++) {
-			const obj = {
-				row: Math.floor(Math.random() * gridSize),
-				column: Math.floor(Math.random() * gridSize)
-			}
-			arr = this.updateGrid(arr, obj.row, obj.column);
-			moves.push(obj);
-		}
-		return { grid: arr, moves };
-	}
-
-	copyGrid = grid => {
-		const arr = new Array(grid.length);
-		for (let i = 0; i < arr.length; i++) {
-			arr[i] = { row: grid[i].row, columns: [ ...grid[i].columns]};
-		}
-		return arr;
+		this.state = initialize(props.gridSize);
 	}
 
 	handleOnNewgame = () => {
-		const { grid, moves } = this.init(this.props);
-		this.setState({ complete: false, grid, moves, clicked: { status: false} });
+		this.setState(initialize(this.props.gridSize));
 	}
 	
-	isInbounds = (row, column) => {
-		if (row < 0 || row >= this.props.gridSize) return false;
-		if (column < 0 || column >= this.props.gridSize) return false;
-		return true;
-	}
-
-	updateGrid = (grid, row, column) => {
-		const array = this.copyGrid(grid);
-		array[row].columns[column].lightson = ! array[row].columns[column].lightson;		// toggle the square that was clicked
-
-		if (this.isInbounds(row - 1, column)) {
-			array[row - 1].columns[column].lightson = ! array[row - 1].columns[column].lightson;
-		}
-
-		if (this.isInbounds(row, column - 1)) {
-			array[row].columns[column - 1].lightson = ! array[row].columns[column - 1].lightson;
-		}
-
-		if (this.isInbounds(row, column + 1)) {
-			array[row].columns[column + 1].lightson = ! array[row].columns[column + 1].lightson;
-		}
-
-		if (this.isInbounds(row + 1, column)) {
-			array[row + 1].columns[column].lightson = ! array[row + 1].columns[column].lightson;
-		}
-		return array;
-	}
-
-	isComplete = grid => {
-		console.log('>>> isComplete; grid ', grid);
-		for (let i = 0; i < grid.length; i++) {
-			const row = grid[i];
-			for (let j = 0; j < row.columns.length; j++) {
-				const column = row.columns[j];
-				if (column.lightson) {
-					console.log('<<< isComplete; false');
-					return false;
-				}
-			}
-		}
-		console.log('<<< isComplete; TRUE');
-		return true;
-	}
-
 	handleOnKeyPressed = (id, row, column) => {
 		console.log('Board::handleOnKeyPressed; id ', id, ' row ', row, ' column ', column);
 		this.setState({ clicked: { status: true, row, column }});
 		setTimeout(() => {
 			this.executeMove(row,column);
-		}, 1000);
+		}, 1500);
 	}
 
 	executeMove = (row, column) => {
 		console.log('Board::executeMove; row ', row, ' column ', column);
 		
 		this.setState(prevState => {
-			const grid = this.updateGrid(prevState.grid, row, column);
+			const grid = updateGrid(prevState.grid, row, column);
 			const moves = [ ...prevState.moves, { row, column }]
 			return (
-				{ complete: this.isComplete(grid) , grid, moves, clicked: { status: false } }
+				{ complete: isComplete(grid) , grid, moves, clicked: { status: false } }
 			)
 		});
 	}
---
+++ src/utils.js
@@ -0,0 +1,78 @@
+// 
+
+/* eslint-disable no-plusplus */
+
+export function copyGrid(grid) {
+	const arr = new Array(grid.length);
+	for (let i = 0; i < arr.length; i++) {
+		arr[i] = { row: grid[i].row, columns: [ ...grid[i].columns]};
+	}
+	return arr;
+}
+
+export function	isComplete(grid) {
+	console.log('>>> isComplete; grid ', grid);
+	for (let i = 0; i < grid.length; i++) {
+		const row = grid[i];
+		for (let j = 0; j < row.columns.length; j++) {
+			const column = row.columns[j];
+			if (column.lightson) {
+				console.log('<<< isComplete; false');
+				return false;
+			}
+		}
+	}
+	console.log('<<< isComplete; TRUE');
+	return true;
+}
+
+function isInbounds(length, row, column) {
+	if (row < 0 || row >= length) return false;
+	if (column < 0 || column >= length) return false;
+	return true;
+}
+
+export function updateGrid(grid, row, column) {
+	const array = copyGrid(grid);
+	array[row].columns[column].lightson = ! array[row].columns[column].lightson;		// toggle the square that was clicked
+
+	if (isInbounds(grid.length, row - 1, column)) {
+		array[row - 1].columns[column].lightson = ! array[row - 1].columns[column].lightson;
+	}
+
+	if (isInbounds(grid.length, row, column - 1)) {
+		array[row].columns[column - 1].lightson = ! array[row].columns[column - 1].lightson;
+	}
+
+	if (isInbounds(grid.length, row, column + 1)) {
+		array[row].columns[column + 1].lightson = ! array[row].columns[column + 1].lightson;
+	}
+
+	if (isInbounds(grid.length, row + 1, column)) {
+		array[row + 1].columns[column].lightson = ! array[row + 1].columns[column].lightson;
+	}
+	return array;
+}
+
+export function initialize(gridSize) {
+	let arr = new Array(gridSize);
+	for (let i = 0; i < arr.length; i++) {
+		const columns = new Array(gridSize);
+		for (let j = 0; j < columns.length; j++) {
+			columns[j] = { id: i * arr.length + j, column: j, lightson: false };
+		}
+		arr[i] = { row: i, columns };
+	}
+	const moves = [];
+	// const maxMoves = props.gridSize ** 2;
+	const maxMoves = 2;
+	for (let i = 0; i < maxMoves; i++) {
+		const obj = {
+			row: Math.floor(Math.random() * gridSize),
+			column: Math.floor(Math.random() * gridSize)
+		}
+		arr = updateGrid(arr, obj.row, obj.column);
+		moves.push(obj);
+	}
+	return { grid: arr, moves, complete: false, clicked: { status: false } };
+}
