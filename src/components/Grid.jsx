df603c400cb5269a5f45026620e79e2a5eaf1945
johnvincentio <john@johnvincent.io>
2022-02-16 22:57:37 -0500

    aria

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -8,46 +8,46 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 class Grid extends React.Component {
-
 	onHandleClick = (id, row, col) => {
 		// console.log('onHandleClick; id ', id, ' row ', row, ' col ', col);
 		this.props.onKeyPressed(row, col);
-	}
+	};
 
 	render() {
 		// console.log('Grid::render(); now ', Date.now(), ' this.props ', this.props);
 		const { grid, disableGrid, clicked, solveit } = this.props;
 		return (
 			<div className="grid">
-				{grid.map((row) => {
-					return (
-						<div key={row.row} className="grid--row">
-							{row.columns.map((column) => {
-								const animation = 
-											(solveit || (! disableGrid && clicked.status))
-											&& clicked.row === row.row 
-											&& clicked.column === column.column;
-								// console.log('animation ', animation);
-								const clz = classnames(
-									`grid--square`, 
-									column.lightson ? `on` : `off`,
-									animation ? `animate` : ``,
-									disableGrid || clicked.status ? `disableButton` : `enableButton`);
-								// console.log('clz ', clz);
-								return (
-									<button 
-										type="button"
-										key={column.id}
-										className={clz}
-										onClick={() => this.onHandleClick(column.id, row.row, column.column)}
-										disabled={disableGrid || clicked.status}
-									>
-										{/* {column.id}, {row.row}, {column.column}, {column.lightson ? "on" : "off"} */}
-									</button>
-								)})}
-						</div>
-					)
-				})}
+				{grid.map((row) => (
+					<div key={row.row} className="grid--row">
+						{row.columns.map((column) => {
+							const animation =
+								(solveit || (!disableGrid && clicked.status)) &&
+								clicked.row === row.row &&
+								clicked.column === column.column;
+							// console.log('animation ', animation);
+							const clz = classnames(
+								`grid--square`,
+								column.lightson ? `on` : `off`,
+								animation ? `animate` : ``,
+								disableGrid || clicked.status ? `disableButton` : `enableButton`
+							);
+							// console.log('clz ', clz);
+							return (
+								<button
+									type="button"
+									key={column.id}
+									className={clz}
+									onClick={() => this.onHandleClick(column.id, row.row, column.column)}
+									disabled={disableGrid || clicked.status}
+									aria-label={`square-${row.row}-${column.column}`}
+								>
+									{/* {column.id}, {row.row}, {column.column}, {column.lightson ? "on" : "off"} */}
+								</button>
+							);
+						})}
+					</div>
+				))}
 			</div>
 		);
 	}
@@ -74,6 +74,6 @@ Grid.propTypes = {
 		column: PropTypes.number
 	}).isRequired,
 	solveit: PropTypes.bool.isRequired
-}
+};
 
 export default Grid;
